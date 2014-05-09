26684aece4997f750a16889b653645043fc03666
Amy Hunter <amyrhunter@gmail.com>
2014-05-08 19:54:48 -0700

    User index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		@user.save
+		redirect_to root_path
 	end
 	
 	def edit
@@ -15,6 +16,7 @@ class UsersController < ApplicationController
 	end
 	
 	def index
+		@users = User.all
 	end
 	
 	def show
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ resources :users
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
