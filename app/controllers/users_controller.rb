c43fa52806c7556527eecdf4eaf34171e2eba97d
Amy Hunter <amyrhunter@gmail.com>
2014-05-08 20:23:39 -0700

    Edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,13 @@ class UsersController < ApplicationController
 	end
 	
 	def edit
+		@user = User.find(params[:id])
 	end
 	
 	def update
+		@user = User.find(params[:id])
+		@user.update(user_params)
+		redirect_to root_path
 	end
 	
 	def index
