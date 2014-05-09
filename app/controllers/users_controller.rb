7b8712115efc9ada199f51fda152f4ee91e83adc
Amy Hunter <amyrhunter@gmail.com>
2014-05-08 20:41:34 -0700

    Destroy users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,9 +24,13 @@ class UsersController < ApplicationController
 	end
 	
 	def show
+		@user = User.find(params[:id])
 	end
 	
 	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to root_path
 	end
 
 private
